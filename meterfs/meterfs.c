b2396922b1938ff8dd7399cd6694322ef619f7cf
Pawel Pisarczyk <pawel.pisarczyk@phoesys.com>
2017-07-03 03:07:03 +0200

    Adding vfork test

--- meterfs/meterfs.c
+++ meterfs/meterfs.c
@@ -8,14 +8,23 @@
 
 int main(void)
 {
-	printf("metetrfs: %d %p\n", 1, main);
+	char *m = "-\\|/";
+	unsigned int i = 0;
+
+	printf("metetrfs: Starting, main is at %p\n", main);
 
 	if (!vfork()) {
-		printf("I'm child!\n");
-		for (;;);
+		printf("I'm child:  ");
+		for (;;) {
+			printf("\b%c", m[i++ % 4]);
+			usleep(100);
+		}
 	}
 
-for (;;);
+	for (;;) {
+		printf("\r%c", m[i++ % 4]);
+		usleep(100);
+	}
 
 	malloc(100);
 
