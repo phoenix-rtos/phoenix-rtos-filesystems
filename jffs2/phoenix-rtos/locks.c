ddc07d92bdbe0c88da89d885c90ab557614cd237
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2022-07-13 17:23:25 +0200

    jffs2: implement RW semaphore as mutex to fix priority inversion issues
    Previous implementation starved threads due to priority inversion. Proper implementation (with no starvation and enabled priority inheritance) to be added in kernel
    
    JIRA: DTR-318

--- jffs2/phoenix-rtos/locks.c
+++ jffs2/phoenix-rtos/locks.c
@@ -16,50 +16,21 @@
 #include "../phoenix-rtos.h"
 #include "locks.h"
 
-
-void up_read(struct rw_semaphore *sem)
-{
-	mutexLock(sem->lock);
-
-	if (sem->cnt < 0x7fffffff) sem->cnt++;
-
-	if (sem->wwait)
-		condSignal(sem->wcond);
-	else if (sem->rwait)
-		condSignal(sem->rcond);
-
-	mutexUnlock(sem->lock);
-}
-
+/*
+ * Note: temporary RW semaphore implementation as mutex
+ * It doesn't allow for multiple concurrent readers but enables priority inheritance
+ * Fixes issues with both readers and writers being starved due to priority inversion
+ * To be replaced by RW semaphore implementation in kernel
+ */
 
 void down_read(struct rw_semaphore *sem)
 {
 	mutexLock(sem->lock);
-
-	sem->rwait++;
-	while (sem->wwait)
-		condWait(sem->rcond, sem->lock, 0);
-
-	while (!sem->cnt)
-		condWait(sem->rcond, sem->lock, 0);
-
-	sem->rwait--;
-	if (sem->cnt) sem->cnt--;
-
-	mutexUnlock(sem->lock);
 }
 
 
-void up_write(struct rw_semaphore *sem)
+void up_read(struct rw_semaphore *sem)
 {
-	mutexLock(sem->lock);
-	sem->cnt = 0x7fffffff;
-
-	if (sem->wwait)
-		condSignal(sem->wcond);
-	else if (sem->rwait)
-		condSignal(sem->rcond);
-
 	mutexUnlock(sem->lock);
 }
 
@@ -67,38 +38,30 @@ void up_write(struct rw_semaphore *sem)
 void down_write(struct rw_semaphore *sem)
 {
 	mutexLock(sem->lock);
+}
 
-	sem->wwait++;
-	while (sem->cnt != 0x7fffffff)
-		condWait(sem->wcond, sem->lock, 0);
 
-	sem->wwait--;
-	sem->cnt = 0;
-
+void up_write(struct rw_semaphore *sem)
+{
 	mutexUnlock(sem->lock);
 }
 
 
 void init_rwsem(struct rw_semaphore *sem)
 {
-	if (sem == NULL)
+	if (sem == NULL) {
 		return;
+	}
 
 	mutexCreate(&sem->lock);
-	condCreate(&sem->wcond);
-	condCreate(&sem->rcond);
-	sem->cnt = 0x7fffffff;
-	sem->rwait = 0;
-	sem->wwait = 0;
 }
 
 
 void exit_rwsem(struct rw_semaphore *sem)
 {
-	if (sem == NULL)
+	if (sem == NULL) {
 		return;
+	}
 
 	resourceDestroy(sem->lock);
-	resourceDestroy(sem->wcond);
-	resourceDestroy(sem->rcond);
 }
