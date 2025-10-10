7d8bff5a2d5fe1ea27ccfd6e2952ba8e369952a6
Adam Greloch <adam.greloch@phoenix-rtos.com>
2025-10-10 14:28:26 +0200

    !meterfs: add (fast) meterfs_erase operation
    breaking: changes the structure of fileheader_t
    
    JIRA: NIL-920

--- meterfs/files.h
+++ meterfs/files.h
@@ -24,20 +24,26 @@
 
 
 typedef struct {
-	unsigned int nvalid:1;
-	unsigned int no:31;
+	unsigned int nvalid : 1;
+	unsigned int no : 31;
 } __attribute__((packed)) index_t;
+
+_Static_assert(sizeof(index_t) <= HGRAIN);
 
 
 typedef struct {
 	unsigned int sector;
-	uint32_t sectorcnt;
 	uint32_t filesz;
 	uint32_t recordsz;
 	char name[8];
-	uint32_t uid;    /* Unique file id, incremented on file header update */
-	uint16_t ncrypt; /* uint16_t for backward compatibility - so checksum is the same */
+	uint32_t uid;     /* Unique file id, incremented on file header update */
+	uint32_t firstid; /* First entry id of current file contents - any entries with lower id are ignored */
+	uint32_t sectorcnt : 17;
+	uint32_t ncrypt : 1;
+	uint32_t unused : 14; /* Unused bits for future use */
 } __attribute__((packed)) fileheader_t;
+
+_Static_assert(sizeof(fileheader_t) <= HGRAIN);
 
 
 typedef struct {
@@ -47,12 +53,16 @@ typedef struct {
 	unsigned char magic[4];
 } __attribute__((packed)) header_t;
 
+_Static_assert(sizeof(header_t) <= HGRAIN);
+
 
 typedef struct {
 	index_t id;
 	uint32_t checksum;
 	unsigned char data[];
 } __attribute__((packed)) entry_t;
+
+_Static_assert(sizeof(entry_t) <= HGRAIN);
 
 
 typedef struct {
